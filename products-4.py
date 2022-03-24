ef46ee9f2ea78ec6a8696aea7d61a0cd359e95e0
Athena Chang <j8687julie28@gmail.com>
2022-03-24 14:51:09 +0800

    add read file

---
+++ products-4.py
@@ -0,0 +1,28 @@
+# 讀取檔案
+products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if'商品,價格' in line:
+			continue 
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# 讓使用者輸入
+while True:
+	name = input('請輸入商品名稱：')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格：')
+	products.append([name, price])
+print(products)
+
+# 印出所有購買紀錄
+for product in products:
+	print(product[0], '的價格是：', product[1])
+
+# 寫入檔案
+with open('products.csv', 'w', encoding = 'utf-8') as f:  #write寫入模式/read讀取模式
+	f.write('商品,價格\n')
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n')
